9344f82c51186c92fab3267820301a64da027fca
damilola <damiloladolor@gmail.com>
2021-08-03 06:22:54 +0100

    set up view and templatesfor index route

--- sca/core/tests.py
+++ sca/core/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class TestIndexView(SimpleTestCase):
+
+    def test_statusOK(self):
+        response = self.client.get(reverse('core:index'))
+        self.assertEquals(response.status_code, 200)
+
+    def test_template_used(self):
+        response = self.client.get(reverse('core:index'))
+        self.assertTemplateUsed(response, 'core/index.html')
+
+    def test_page_content(self):
+        response = self.client.get(reverse('core:index'))
+        self.assertContains(response, 'Hello world')
+
--- sca_project/settings/dev.py
+++ sca_project/settings/dev.py
@@ -10,4 +10,5 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j@6b^%-&i+f&9fortranetmx=!e#7e)^dn2(np94_b-!ly&xyz'
+# This key is just used for local development
+SECRET_KEY = "j@6b^%-&i+f&9fortranetmx=!e#7e)^dn2(np94_b-!ly&xyz"
