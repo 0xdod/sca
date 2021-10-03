4e93b62db291c8076abea01daecdefc12ea7aa41
damilola dolor <damiloladolor@gmail.com>
2021-10-03 12:57:07 +0100

    added whitenoise and psql

--- sca_project/settings/prod.py
+++ sca_project/settings/prod.py
@@ -1,16 +1,16 @@
 import os
 
 from .base import *
+import dj_database_url
 
 DEBUG = False
 
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+DATABASES = {}
 
-DATABASES = {
-        'default':  {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': 'sca',
-            'USER': 'admin',
-            'PASSWORD': ''
-            },
-        }
+DATABASES['default'] = dj_database_url.parse(os.environ["HEROKU_POSTGRESQL_BRONZE_URL"], conn_max_age=601) 
+
+MIDDLEWARE+='whitenoise.middleware.WhiteNoiseMiddleware'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
