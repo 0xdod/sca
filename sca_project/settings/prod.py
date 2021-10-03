fdc1f4f03ae7feffa9eae80d3cb9583f8341d2b2
damilola dolor <damiloladolor@gmail.com>
2021-10-03 13:11:03 +0100

    edited middleware

--- sca_project/settings/prod.py
+++ sca_project/settings/prod.py
@@ -10,7 +10,7 @@ DATABASES = {}
 
 DATABASES['default'] = dj_database_url.parse(os.environ["HEROKU_POSTGRESQL_BRONZE_URL"], conn_max_age=601) 
 
-MIDDLEWARE+='whitenoise.middleware.WhiteNoiseMiddleware'
+MIDDLEWARE+=['whitenoise.middleware.WhiteNoiseMiddleware']
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
