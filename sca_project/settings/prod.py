8e07a6e3c954784fb2ed8910df01891d72545d0c
damilola dolor <damiloladolor@gmail.com>
2021-10-22 06:39:41 +0100

    no shit

--- sca_project/settings/prod.py
+++ sca_project/settings/prod.py
@@ -3,13 +3,10 @@ import os
 from .base import *
 import dj_database_url
 
-import django_heroku
-
-django_heroku.settings(locals())
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['scacademy.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 DATABASES = {}
 
 DATABASES['default'] = dj_database_url.parse(os.environ["HEROKU_POSTGRESQL_BRONZE_URL"], conn_max_age=601) 
