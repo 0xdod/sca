68dc2f14c43688dd07cedb3662e11900c803e527
damilola dolor <damiloladolor@gmail.com>
2021-10-22 01:19:10 +0100

    added allowed hosts

--- sca_project/settings/prod.py
+++ sca_project/settings/prod.py
@@ -5,7 +5,7 @@ import dj_database_url
 
 DEBUG = False
 
-ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
+ALLOWED_HOSTS = ['scacademy.herokuapp.com']
 DATABASES = {}
 
 DATABASES['default'] = dj_database_url.parse(os.environ["HEROKU_POSTGRESQL_BRONZE_URL"], conn_max_age=601) 
