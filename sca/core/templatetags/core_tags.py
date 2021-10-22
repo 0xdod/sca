c262bbe56aec4ffce5a53f1dffb35d61c4cdd759
damilola dolor <damiloladolor@gmail.com>
2021-10-22 08:51:17 +0100

    made homepage responsive

--- sca/core/templatetags/core_tags.py
+++ sca/core/templatetags/core_tags.py
@@ -2,7 +2,11 @@ from django import template
 
 register = template.Library()
 
+
 @register.simple_tag
 def call_method(obj, method_name, *args):
-	method = getattr(obj, method_name)
-	return method(*args)
+    try:
+        method = getattr(obj, method_name)
+        return method(*args)
+    except AttributeError:
+        pass
