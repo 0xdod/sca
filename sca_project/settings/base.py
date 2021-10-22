9f0b55ac74fad7182bbd1584cf86af5f2c99dfa4
damilola dolor <damiloladolor@gmail.com>
2021-10-22 01:17:41 +0100

    bleh

--- sca_project/settings/base.py
+++ sca_project/settings/base.py
@@ -131,10 +131,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/",
+    BASE_DIR / "staticfiles/",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATIC_ROOT = BASE_DIR / "static/"
 
 # Media files, files uploaded by users
 
