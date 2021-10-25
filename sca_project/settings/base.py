ee7219ca7eccd034754f4befdf0ce9bb41375e53
damilola dolor <damiloladolor@gmail.com>
2021-10-25 22:32:07 +0100

    added static_root settings

--- sca_project/settings/base.py
+++ sca_project/settings/base.py
@@ -131,10 +131,10 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
-    BASE_DIR / "static/",
+    BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / "staticfiles/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Media files, files uploaded by users
 
