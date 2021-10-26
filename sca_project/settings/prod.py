127b024ffe3708ed12c77b7199240e5afe345503
damilola dolor <damiloladolor@gmail.com>
2021-10-26 14:57:56 +0100

    added cloudinary storage for heroku

--- sca_project/settings/prod.py
+++ sca_project/settings/prod.py
@@ -1,3 +1,4 @@
+from logging import INFO
 import os
 
 from .base import *
@@ -6,7 +7,21 @@ import dj_database_url
 
 DEBUG = False
 
+INSTALLED_APPS += [
+    'cloudinary_storage',
+    'cloudinary',
+]
+
+
 ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS')]
 DATABASES = {}
 
-DATABASES['default'] = dj_database_url.parse(os.environ["HEROKU_POSTGRESQL_BRONZE_URL"], conn_max_age=601) 
+DATABASES['default'] = dj_database_url.parse(os.environ["HEROKU_POSTGRESQL_BRONZE_URL"],
+                                             conn_max_age=601)
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUD_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUD_API_SECRET'),
+}
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
