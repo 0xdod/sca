888b8e28b1d43d993a7e4fe5735d286d9446321a
damilola <damiloladolor@gmail.com>
2021-12-09 20:44:51 +0100

    makes changes in templates

--- sca/core/views.py
+++ sca/core/views.py
@@ -16,6 +16,7 @@ class IndexView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["latest_courses"] = Course.objects.all()[:3]
+        context["reviews"] = [1, 2, 3]
         return context
 
 
